0c679115ea9f4f25e4d6233bb0eeae8890b8d760
Maik Knof <knofm@hs-weingarten.de>
2025-03-29 11:54:48 +0100

    copy paste issue

--- simple_grasping_world/src/params.cpp
+++ simple_grasping_world/src/params.cpp
@@ -7,8 +7,8 @@ void load_parameters(NodeConfig &config, rclcpp::Node *node)
     config.common.world_path = declare_and_get_parameter<std::string>("common.world_path", "", node, "common.world_path");
     config.common.tf_prefix = declare_and_get_parameter<std::string>("common.tf_prefix", "world_", node, "common.tf_prefix");
 
-    // furnitureects parameters
-    config.furnitureects.voxel_resolution = declare_and_get_parameter<float>("furnitureects.voxel_resolution", 0.1f, node, "furnitureects.voxel_resolution");
-    config.furnitureects.leaf_size = declare_and_get_parameter<float>("furnitureects.leaf_size", 0.01f, node, "furnitureects.leaf_size");
-    config.furnitureects.grid_size = declare_and_get_parameter<int>("furnitureects.grid_size", 50, node, "furnitureects.grid_size");
+    // furnitures parameters
+    config.furnitures.voxel_resolution = declare_and_get_parameter<float>("furnitures.voxel_resolution", 0.1f, node, "furnitures.voxel_resolution");
+    config.furnitures.leaf_size = declare_and_get_parameter<float>("furnitures.leaf_size", 0.01f, node, "furnitures.leaf_size");
+    config.furnitures.grid_size = declare_and_get_parameter<int>("furnitures.grid_size", 50, node, "furnitures.grid_size");
 }
